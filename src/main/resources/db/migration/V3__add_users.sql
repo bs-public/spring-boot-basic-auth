cc0920da5eca26e04b125ae0ba5df9e96c1ebe78
bs <26665808+bs-public@users.noreply.github.com>
2025-07-13 23:44:09 +1000

    Add schema and flyway support

---
+++ src/main/resources/db/migration/V3__add_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (email, first_name, last_name, password, role_id) VALUES ('test.user1@mail.com', 'Test1', 'User1', 'password1', 2);
+INSERT INTO user (email, first_name, last_name, password, role_id) VALUES ('test.user2@mail.com', 'Test2', 'User2', 'password2', 2);
+INSERT INTO user (email, first_name, last_name, password, role_id) VALUES ('admin@mail.com', 'Admin', 'User', 'password', 1);
